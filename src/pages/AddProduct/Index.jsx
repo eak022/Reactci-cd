3ad5e1f2a79fb932e2f4baa6d89dd65378e03567
eak022 <eakeakkachai71180@gmail.com>
2025-04-03 15:45:27 +0700

    update

--- src/pages/AddProduct/Index.jsx
+++ src/pages/AddProduct/Index.jsx
@@ -11,7 +11,7 @@ const Index = () => {
     price: "",
     description: "",
     condition: "",
-    // postAd: "", // อย่าลืมเพิ่ม Backend
+    // postAd: "", // อย่าลืมเพิ่ม Backendนะ
   });
 
   const handlePostTypeChange = (e) => {
